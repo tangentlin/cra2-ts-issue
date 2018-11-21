10c675e1f5aa128cdee25f2894071e292d827ea0
Tangent Lin <tangent@usa.net>
2018-11-21 10:44:54 -0500

    video-react stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BigPlayButton, ControlBar, Player } from 'video-react';
+import "video-react/dist/video-react.css";
 import './App.css';
+import logo from './logo.svg';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                </header>
+
+
+                <Player
+                    src="http://media.w3.org/2010/05/bunny/movie.mp4"
+                    fluid={false}
+                    height="100%">
+                    <ControlBar autoHide={true}/>
+                    <BigPlayButton position="hide"/>
+                </Player>
+            </div>
+
+
+        );
+    }
 }
 
 export default App;
---
+++ src/video-react.d.ts
@@ -0,0 +1,83 @@
+import * as React from 'react';
+
+declare module 'video-react' {
+    export type Length = number | string | 'auto';
+
+    export interface IPlayerProps {
+        fluid?: boolean;
+        width?: Length;
+        height?: Length;
+        src: string;
+        poster?: string;
+        preload?: 'none' | 'metadata' | 'auto';
+        muted?: boolean; // default false
+        playsInline?: boolean;
+        aspectRatio?: 'auto' | '16:9' | '4:3';
+        autoPlay?: boolean;
+        startTime?: number;
+        loop?: boolean;
+    }
+
+    export interface IPlayerState {
+        currentSrc: string;
+        duration: number;
+        currentTime: number;
+        seekingTime: number;
+        buffered: object;
+        waiting: boolean;
+        seeking: boolean;
+        paused: boolean;
+        autoPaused: boolean;
+        ended: boolean;
+        playbackRate: number;
+        muted: boolean;
+        volume: number;
+        readyState: string;
+        networkState: string;
+        videoWidth: number;
+        videoHeight: number;
+        hasStated: boolean;
+        userActivity: boolean;
+        isActive: boolean;
+        isFullscreen: boolean;
+        videoId: string;
+    }
+
+    export type StateChangeDelegate = (state: IPlayerState, previousState: IPlayerState) => void;
+
+    export class Player extends React.Component<IPlayerProps, {}> {
+        public playbackRate: number;
+        public muted: boolean;
+        public volume: number;
+        public videoWidth: number;
+        public videoHeight: number;
+        public video: any;
+
+        public getState(): IPlayerState;
+        public play(): void;
+        public pause(): void;
+        public load(): void;
+        public addTextTrack(): void;
+        public canPlayType(): boolean;
+        public seek(time: number): void;
+        public forward(seconds: number): void;
+        public replay(seconds: number): void;
+        public toggleFullscreen(): void;
+        public subscribeToStateChange(listener: StateChangeDelegate): void;
+    }
+
+    export interface IControlBarProps {
+        autoHide?: boolean;
+        disableCompletely?: boolean;
+    }
+
+    export class ControlBar extends React.Component<IControlBarProps, {}> {
+    }
+
+    export interface IBigPlayButtonProps {
+        position?: string; // hide, left, top, influenced by CSS class names video-react-big-play-button-$position
+    }
+
+    export class BigPlayButton extends React.Component<IBigPlayButtonProps, {}> {
+    }
+}
